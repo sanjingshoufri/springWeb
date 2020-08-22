0cbd8e45271d2e2bbeca0ff5818a1768f71745e0
TANNER <2303386665@qq.com>
2020-08-22 17:41:35 +0800

    maven spring boot

--- src/main/java/tn/test/spring_boot/demo/HelloController.java
+++ src/main/java/tn/test/spring_boot/demo/HelloController.java
@@ -2,11 +2,26 @@ package tn.test.spring_boot.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.lang3.StringUtils;
+
 
 @RestController
 public class HelloController {
 	@RequestMapping("/hello")
 	public String hello(){
 		return "hello";
+	}
+	
+	@RequestMapping("/notice")
+	public String noticeList(){
+		// notice list from db-table
+		
+		
+		// array turn into json format
+        String[] names = {"taonian", "xiaoyu", "chu"};
+        
+        StringUtils.
+        
+        return "hello";
 	}
 }
